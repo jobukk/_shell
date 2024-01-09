0c6e1c28d21e8756e4eba848ab60e978d4168549
JobKoech610 <jobkoech610@gmail.com>
2024-01-09 23:30:14 +0300

    shell-fix

--- shell.h
+++ shell.h
@@ -2,17 +2,33 @@
 #define SHELL_H
 
 #include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
 
-/* Helper Funcitons */
+/* Execute Functions */
+int startsWithForwardSlash(char const *str);
 char *get_file_path(char *file_name);
 char *get_file_loc(char *path, char *file_name);
-int startsWithForwardSlash(const char *str);
+void execute_command(char *input, char *argv[], char **env);
+char *read_input(void);
+void print_prompt(void);
+int tokenize_input(char *input, char *args[]);
 
+/* Built in Functions */
+int handle_builtin_commands(char **args,
+		int num_args, char *input,
+		char **env);
+void print_env(char **env);
+void handle_cd(char **args, int num_args);
+void handle_exit(char *input, int exit_status);
+int shell_exit(char **args, char *input);
 
+/* Handle various symbols */
+char *handle_comment(char *input);
 #endif /* SHELL_H */
