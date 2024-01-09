0ad7676e4de9bd2614591aec11f7684277a66685
JobKoech610 <jobkoech610@gmail.com>
2024-01-09 23:22:57 +0300

    shell

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <string.h>
+
+/* Helper Funcitons */
+char *get_file_path(char *file_name);
+char *get_file_loc(char *path, char *file_name);
+int startsWithForwardSlash(const char *str);
+
+
+#endif /* SHELL_H */
